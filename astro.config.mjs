224c7dc918d8488be62564367ad892a3a367ec2a
Suzza <hello+git@susansilver.net>
2023-03-18 02:51:54 -0700

    Revert "Created blog redirect"
    This reverts commit 8a63a085d664ea285240a9c581e3fab708bf3421.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,11 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import partytown from "@astrojs/partytown";
 
-import netlify from "@astrojs/netlify/functions";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://beautyofmathematics.com',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()],
-  output: "server",
-  adapter: netlify()
+  integrations: [mdx(), sitemap(), tailwind(), svelte()]
 });
