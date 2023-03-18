8a63a085d664ea285240a9c581e3fab708bf3421
Suzza <hello+git@susansilver.net>
2023-03-18 02:37:30 -0700

    Created blog redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import partytown from "@astrojs/partytown";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://beautyofmathematics.com',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  output: "server",
+  adapter: netlify()
 });
