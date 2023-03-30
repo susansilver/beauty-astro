b95724946472aa2d9bc339fb11301f791bc62262
Suzza <hello+git@susansilver.net>
2023-03-30 10:35:56 -0700

    Added integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
+import deadLinks from "astro-dead-links";
+import compress from "astro-compress";
+
+import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://beautyofmathematics.com',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+  integrations: [mdx(), sitemap(), tailwind(), svelte(), deadLinks(), compress(), compressor()]
 });
