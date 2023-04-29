eb82ad5a84cc6cdb973ac049341fb7be0db2cd8a
Suzza <hello+git@susansilver.net>
2023-04-29 13:15:59 -0700

    update rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -15,7 +15,7 @@ export async function get(context) {
 			customData: post.data.customData,
 			// Compute RSS link from post `slug`
 			// This example assumes all posts are rendered as `/blog/[slug]` routes
-			link: `${post.slug}/`,
+			link: `/${post.slug}/`,
 		  })),
 		});
 	  }
